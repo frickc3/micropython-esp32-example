b8ba8aec443e80d095cf9a4765e17835e9e6f3e9
frickc3 <frickc3@gmail.com>
2023-01-03 10:20:31 -0500

    Second commit with main.py and config.py changes

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
-WIFI_SSID = 'wifi ssid'
-WIFI_PASSWD = 'wifi password'
-SOME_API_KEY = 'myapikey'
+WIFI_SSID = ''
+WIFI_PASSWD = ''
+WIFI_TXPOWER = ''
+#WIFI_TXPOWER  = '8.5'               # Used if wifi connect hangs
+VERSION = '1.0.0'                    # For version display
+SOME_API_KEY = 'myapikey'            # Random config value
--- main.py
+++ main.py
@@ -6,21 +6,6 @@ import os
 import ssd1306
 import time
 
-wlan = network.WLAN(network.STA_IF) # create station interface
-if wlan.isconnected() != True:
-    wlan.active(False)
-    wlan.active(True)       # activate the interface
-    wlan.config(txpower=8.5)  #set to 8.5dBm
-    wlan.isconnected()      # check if the station is connected to an AP
-    time.sleep_ms(500)
-    if not wlan.isconnected():
-      print('connecting to network...')
-      wlan.connect(config.WIFI_SSID, config.WIFI_PASSWD) # connect to an AP
-      time.sleep_ms(500)
-      while not wlan.isconnected():
-        pass
-print('network config:', wlan.ifconfig())
-
 oled_width = 128
 oled_height = 64
 # OLED reset pin
@@ -37,9 +22,37 @@ i2c = SoftI2C(scl=i2c_scl, sda=i2c_sda)
 # Create the display object
 oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
 oled.fill(0)
+
+wlan = network.WLAN(network.STA_IF) # create station interface
+if wlan.isconnected() != True:
+    wlan.active(False)
+    wlan.active(True)       # activate the interface
+    try:
+        if config.WIFI_TXPOWER:
+            wlan.config(txpower=8.5)  #set to 8.5dBm
+    except:
+        pass
+    wlan.isconnected()      # check if the station is connected to an AP
+    time.sleep_ms(500)
+    if not wlan.isconnected():
+      print('connecting to network...')
+      oled.text('network connecting',0, 0)
+      oled.show()
+      wlan.connect(config.WIFI_SSID, config.WIFI_PASSWD) # connect to an AP
+      time.sleep_ms(500)
+      while not wlan.isconnected():
+        time.sleep_ms(100)  
+        pass
+print('network config:', wlan.ifconfig())
+
+oled.fill(0)
 oled.text(wlan.ifconfig()[0], 0, 0)
 oled.text('HELLO WiFi ESP32', 0, 25)
-oled.text('Version 1.0.0', 0, 55)
+try:
+    if config.VERSION:
+        oled.text('Version: '+config.VERSION, 0, 55)
+except:    
+    oled.text('no version found', 0, 55)
   
 #oled.line(0, 0, 50, 25, 1)
 oled.show()
